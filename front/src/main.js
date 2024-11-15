fa4f529eee5f5918a4bdde752dc21181c5061cd0
aga1215 <sanghwa1215@naver.com>
2024-11-15 22:34:48 +0900

    feat: 로그인, 회원가입, 비밀번호 찾기 페이지 생성

--- front/src/main.js
+++ front/src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue';
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(Antd);
+
+app.mount('#app');
